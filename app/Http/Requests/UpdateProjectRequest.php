22d499a4e721787c52a9e423610a5d27310a7f4d
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-04-24 18:38:01 +0200

    fixed   error for edit.blade

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -22,7 +22,24 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            // vlaidation rules
+
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'img' => 'required|string',
+            'tecnologies' => 'required|string',
+            'link' => 'required|string',
+        ];
+    }
+
+    public function message(): array
+    {
+        return [
+            'title.required' => 'Title is required',
+            'description.required' => 'Description is required',
+            'img.required' => 'Image is required',
+            'type.required' => 'Type is required',
+            'link.required' => 'Link is required',
         ];
     }
 }
